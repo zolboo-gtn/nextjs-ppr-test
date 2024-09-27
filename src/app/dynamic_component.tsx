e323551f77516ee5949cb13898e361153dd1d627
zolboo-gtn <72186937+zolboo-gtn@users.noreply.github.com>
2024-09-27 13:44:43 +0800

    misc

--- src/app/dynamic_component.tsx
+++ src/app/dynamic_component.tsx
@@ -1,6 +1,9 @@
+import { cookies } from "next/headers";
+
 export const DynamicComponent: React.FC<{ timeout: number }> = async ({
   timeout,
 }) => {
+  const session = cookies().get("session")?.value;
   await new Promise((resolve) => setTimeout(resolve, timeout));
 
   return <div>{new Date().toISOString()}</div>;
